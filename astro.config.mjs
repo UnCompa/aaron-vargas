5c7bf276a0f6875d8f2f1a429ea2afe4320858b5
UnCompa <brandonddxd@gmail.com>
2023-10-08 12:32:40 -0500

    Teniendo fe

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()],
   site: 'https://uncompa.github.io',
-  base: '/aaron-vargas',
+  base: '/aaron-vargas/',
 });
